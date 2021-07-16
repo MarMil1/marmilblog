c9c98a7cc5bb82fc202bd7dd81d79f7017fda343
MarMil1 <milosavljevic.marko@outlook.com>
2021-07-15 22:28:50 -0500

    Implemented login/logout and session tracking using cookies. Enabled remember me option upon login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in
+      # Log the user in, remember their token and direct to user profile page
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else 
       # Create new render on login page and throw error
-      flash.now[:danger] = 'Invalid email/password combination.'
+      flash.now[:danger] = 'Invalid email or password.'
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to articles_path
   end
 
