17bdb82671b456c1e087e46adc13d6baac5e9c7c
Marko Milosavljevic <markomilosavljevic@Markos-MacBook-Air.local>
2022-08-24 23:00:33 -0500

    Add favorites functionality along with its model. Add create, get and destroy routes and methods for favorites feature. Minor style adjustment.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_173751) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_161745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_173751) do
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id", "created_at"], name: "index_comments_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
