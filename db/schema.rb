3d7fa4b5b384bc3591556edd483edf88cbb6413d
Marko Milosavljevic <markomilosavljevic@Markos-MacBook-Air.local>
2022-09-04 22:51:09 -0500

    Add images to articles to show on only first article of each article list. Change app name to escrit. Minor style adjustments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_043758) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_014526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_043758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comments_count", default: 0, null: false
+    t.text "image_data"
     t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
