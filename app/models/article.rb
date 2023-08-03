f3aa2f35a131c60098d91d34e5f0c9696f07aa4b
Marko Milosavljevic <marko@Markos-MacBook-Pro.local>
2023-08-03 17:05:04 -0500

    Add slug to articles database and url.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  after_validation :set_slug, only: [:create, :update]
   include ImageUploader::Attachment(:image)
 
   belongs_to :user
@@ -10,4 +11,12 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
+
+  def set_slug
+    self.slug = title.to_s.parameterize
+  end 
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 end
