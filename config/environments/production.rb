3e5b319afb975b7b0f67b771e78062399516ce09
Marko Milosavljevic <marko@Markos-MacBook-Pro.local>
2023-08-04 23:42:12 -0500

    Add email secrets config file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  user_name:            ENV['EMAIL_USERNAME'],
-  password:             ENV['EMAIL_PASSWORD'],
+  user_name:            Rails.application.secrets.gmail_username,
+  password:             Rails.application.secrets.gmail_password,
   authentication:       'plain',
   enable_starttls_auto: true,
   open_timeout:         5,
